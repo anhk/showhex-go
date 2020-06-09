f92482a347626ec1085879f1c1f1e73fb7cec9ac
anhk <anhongkui@jd.com>
2020-06-09 15:38:49 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/anhk/showhex-go
+
+go 1.14
---
+++ showhex.go
@@ -0,0 +1,32 @@
+package showhex
+
+import "fmt"
+
+func ShowLine(line []byte) {
+	var i int
+	for i = 0; i < len(line); i++ {
+		fmt.Printf("%02X", line[i])
+	}
+	for ; i < 16; i++ {
+		fmt.Printf("   ")
+	}
+	fmt.Printf("\t")
+	for i = 0; i < len(line); i++ {
+		if line[i] >= ' ' && line[i] < 128 {
+			fmt.Printf("%c", line[i])
+		} else {
+			fmt.Printf(".")
+		}
+	}
+}
+
+func ShowHex(data []byte) {
+	for {
+		if len(data) <= 16 {
+			break
+		}
+		ShowLine(data[:16])
+		data = data[16:]
+	}
+	ShowLine(data)
+}
